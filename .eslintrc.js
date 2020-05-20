07a8ee5717335e9e96192b7243543b4709c76bc5
martins20 <pdesignerpb@gmail.com>
2020-05-20 19:37:26 -0300

    fixing rules and the whole file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,34 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/pretier': 'error',
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-case-declarations': 0,
+    'no-unused-vars': 0,
+    'prettier/pretier': 0,
+    'no-console': 0,
+    'jsx-a11y/label-has-associated-control': 0,
+    'react-hooks/exhaustive-deps': 0,
+    'react/prop-types': 0,
+  },
 };
