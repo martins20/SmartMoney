e707e18d54346300ec2d9c581e7eab7696d4d2de
Paulo César <pdesignerpb@gmail.com>
2020-05-22 19:24:43 -0300

    Config for debbug with ReactTotron

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,6 @@ module.exports = {
     'no-console': 0,
     'jsx-a11y/label-has-associated-control': 0,
     'react-hooks/exhaustive-deps': 0,
-    'react/prop-types': 0,
+    'react/prop-types': 0
   },
 };
